8bc1ed7c66891ec7fc3f19b1a341290669049a69
Jakub Skrzypek <kuba81s@o2.pl>
2020-07-05 17:26:10 +0100

    file structure

--- index.php
+++ index.php
@@ -1,31 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-class Animal
-{
-  private string $family;
-  private string $food;
-
-  public function __construct($family, $food)
-  {
-    $this->family = $family;
-    $this->food = $food;
-  }
-  public function get_family()
-  {
-    return $this->family;
-  }
-  public function set_family(string $family)
-  {
-    $this->family = $family;
-  }
-  public function get_food()
-  {
-    return $this->food;
-  }
-  public function set_food(string $food)
-  {
-    $this->food = $food;
-  }
-}
