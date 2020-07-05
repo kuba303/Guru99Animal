fbed5dc428e984b04ca50fb3eba2a5ed28124dcc
Jakub Skrzypek <kuba81s@o2.pl>
2020-07-05 17:44:22 +0100

    add Lion class

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 // declare(strict_types=1);
 
 require 'Animal.php';
+require 'Cow.php';
+
 
 // $obj = new Animal("mamal", "coronoviurus");
 
+// var_dump($obj);
+
+// $obj = new Cow("aaaa", "bbb");
+// $obj->set_owner('Kuba');
+// $obj->get_owner();
 // var_dump($obj);
